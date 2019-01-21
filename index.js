e3896a99643c02eb5e189f7338f334daae9ce5ee
george.robakidze-at-334057996933 <robakidzegeorge@gmail.com>
2019-01-21 16:06:50 +0200

    Removing express.js and creating log schedule

--- index.js
+++ index.js
@@ -1,90 +1,70 @@
 'use strict'
 
-const serverless = require('serverless-http')
 const AWS = require('aws-sdk')
-const express = require('express')
+const CSV = require('csv')
+const sortObject = require('sort-object')
 
-const app = express()
+let keysOrder = Object.keys(require('./csv-template.js'))
 
-app.get('/', async function(request, response) {
-  const CSV = require('csv')
-  const sortObject = require('sort-object')
+const DynamoDB = new AWS.DynamoDB.DocumentClient()
+const S3 = new AWS.S3()
 
-  let keysOrder = Object.keys(require('./csv-template.js')).sort()
-
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
-  const S3 = new AWS.S3()
-  const getTableContent = function(tableName) {
-    return new Promise(function(resolve, reject) {
-      DynamoDB.scan({ TableName: tableName }, function(err, data) {
-        if(err != null) reject(err)
-        else resolve(data.Items)
-      })
+const getTableContent = function(tableName) {
+  return new Promise(function(resolve, reject) {
+    DynamoDB.scan({ TableName: tableName }, function(err, data) {
+      if(err != null) reject(err)
+      else resolve(data.Items)
     })
-  }
-  const csvTableFormatter = function(data) {
-    let csvTable = []
-    return new Promise(function(resolve, reject) {
-      data.forEach(function(item) {
-        keysOrder.forEach(function(prop) {
-          if(!(prop in item)) item[prop] = null
-        })
-        csvTable.push(Object.values(sortObject(item, keysOrder)))
-      })
-      resolve(csvTable)
+  })
+}
+const csvTableFormatter = function(data) {
+  let csvTable = []
+  data.forEach(function(item) {
+    keysOrder.forEach(function(prop) {
+      if(!(prop in item)) item[prop] = null
     })
-  }
-  const csvStringify = function(table) {
-    return new Promise(function(resolve, reject) {
-      CSV.stringify(table, {
-        header: true,
-        columns: keysOrder
-      }, function(err, data) {
-        if(err != null) reject(err)
-        else resolve(data)
-      })
+    csvTable.push(Object.values(sortObject(item, keysOrder)))
+  })
+  return csvTable
+}
+const csvStringify = function(table) {
+  return new Promise(function(resolve, reject) {
+    CSV.stringify(table, {
+      header: true,
+      columns: Object.values(require('./csv-template.js'))
+    }, function(err, data) {
+      if(err != null) reject(err)
+      else resolve(data)
     })
-  }
+  })
+}
+const csvToS3 = function(csvFile, bucketName) {
+  return new Promise(function(resolve, reject) {
+    let params = {
+      Bucket: bucketName,
+      Key: csvFile.name + '.csv',
+      Body: csvFile.text
+    }
+    S3.upload(params, function(err, data) {
+      if(err != null) reject(err)
+      else resolve(data)
+    })
+  })
+}
 
+module.exports.handler = async function(event, context, callback) {
+  console.log(event)
   try {
-    let tableInfo, csvTable = []
+    let csvTable = []
 
-    tableInfo = await getTableContent(process.env.CRM_CONTACTS_TABLE_ARN.split('/')[1])
-    csvTable = csvTable.concat(await csvTableFormatter(tableInfo))
+    csvTable = csvTable.concat(csvTableFormatter(await getTableContent(process.env.CRM_CONTACTS_TABLE_ARN.split('/')[1])))
+    csvTable = csvTable.concat(csvTableFormatter(await getTableContent(process.env.CRM_GROUPS_TABLE_ARN.split('/')[1])))
+    csvTable = csvTable.concat(csvTableFormatter(await getTableContent(process.env.CRM_UPDATES_TABLE_ARN.split('/')[1])))
+    csvTable = csvTable.concat(csvTableFormatter(await getTableContent(process.env.CRM_COMPANIES_TABLE_ARN.split('/')[1])))
 
-    tableInfo = await getTableContent(process.env.CRM_GROUPS_TABLE_ARN.split('/')[1])
-    csvTable = csvTable.concat(await csvTableFormatter(tableInfo))
-
-    tableInfo = await getTableContent(process.env.CRM_UPDATES_TABLE_ARN.split('/')[1])
-    csvTable = csvTable.concat(await csvTableFormatter(tableInfo))
-
-    tableInfo = await getTableContent(process.env.CRM_COMPANIES_TABLE_ARN.split('/')[1])
-    csvTable = csvTable.concat(await csvTableFormatter(tableInfo))
-
-    var csvString = await csvStringify(csvTable)
+    await csvToS3({name: 'cloud-crm-data-backup', text: await csvStringify(csvTable)}, process.env.UNIVERSAL_STORAGE_ARN.split(':::')[1])
   }
   catch(err) {
     console.log('Error: ' + JSON.stringify(err))
-    response.end('Error')
   }
-
-  let params = {
-    Bucket: process.env.UNIVERSAL_STORAGE_ARN.split(':::')[1],
-    Key: 'cloud-crm-data-backup.csv',
-    Body: csvString
-  }
-  console.log(process.env.UNIVERSAL_STORAGE_ARN)
-
-  S3.upload(params, function (err, data) {
-    if (err) {
-      console.log(JSON.stringify(err))
-      response.end('Error')
-    }
-    else {
-      console.log(JSON.stringify(data))
-      response.end('Success')
-    }
-  })
-})
-
-module.exports.handler = serverless(app)
+}
